3f41e564c0fe00e0a5360ddd63b89478e2870aee
WilliamHsiang <william.hsiang@kellogg.northwestern.edu>
2026-02-05 02:53:01 +0000

    in-class exercise

--- code-along/2-models.rb
+++ code-along/2-models.rb
@@ -12,20 +12,43 @@ Company.destroy_all
 #   (i.e. full CRUD of company data).
 
 new_company = Company.new
-puts new_company
+# puts new_company
 
-new_company["name"] = "Apple"
-new_company["city"] = "Cupertino"
-new_company["state"] = "CA"
-new_company["url"] = "apple.com"
-p new_company
+
 
 # 1a. check out the schema file
 # 1b. check out the model file
 
 # 2. insert new rows in companies table
 
+new_company["name"] = "Apple"
+new_company["city"] = "Cupertino"
+new_company["state"] = "CA"
+new_company["url"] = "apple.com"
+new_company.save
+
+new_company = Company.new
+new_company["name"] = "Amazon"
+new_company["city"] = "Seattle"
+new_company["state"] = "WA"
+new_company["url"] = "amz.com"
+new_company.save
+
+new_company = Company.new
+new_company["name"] = "Anthropic"
+new_company["city"] = "SF"
+new_company["state"] = "CA"
+new_company["url"] = "anthropic.com"
+new_company.save
+
+# p new_company
+puts "Companies: #{Company.all.count}"
+
 # 3. query companies table to find all row with California company
+
+cali_companies = Company.where({ "state" => "CA" })
+# p cali_companies
+puts "CA companies: #{cali_companies.count}"
 
 # 4. query companies table to find single row for Apple
 
--- code-along/3-associations.rb
+++ code-along/3-associations.rb
@@ -12,6 +12,45 @@ Contact.destroy_all
 
 # 1. insert new rows in the contacts table with relationship to a company
 
+apple = Company.find_by({ "name" => "Apple" })
+
+contact = Contact.new
+contact["first_name"] = "Tim"
+contact["last_name"] = "Cook"
+contact["email"] = "tim@apple.com"
+contact["company_id"] = apple["id"]
+contact.save
+
+contact = Contact.new
+contact["first_name"] = "Craig"
+contact["last_name"] = "Fed"
+contact["email"] = "craig@apple.com"
+contact["company_id"] = apple["id"]
+contact.save
+
+amz = Company.find_by({ "name" => "Amazon" })
+
+contact = Contact.new
+contact["first_name"] = "Jeff"
+contact["last_name"] = "Bezos"
+contact["email"] = "jeff@amz.com"
+contact["company_id"] = amz["id"]
+contact.save
+
+puts "Contacts: #{Contact.all.count}"
+
+p contact
+
 # 2. How many contacts work at Apple?
 
+apple_emp = Contact.where({ "company_id" => apple["id"] })
+
+puts "Apple employees: #{apple_emp.count}"
+
 # 3. What is the full name of each contact who works at Apple?
+
+for contact in apple_emp
+  first_name = contact["first_name"]
+  last_name = contact["last_name"]
+  puts "#{first_name} #{last_name}"  
+end
